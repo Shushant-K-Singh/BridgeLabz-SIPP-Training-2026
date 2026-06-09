7e8b9dc993f2be5c061ad30b6f75c310ad4c5dbc
Shushant-K-Singh <shushantkumar860@gmail.com>
2026-06-09 18:57:43 +0530

    [Shushant-K-Singh].Add completed all the questions of Programming Elements level1

---
+++ core-java-practice/gcr-codebase/Programming_Elements/level1/HeightConversion.java
@@ -0,0 +1,20 @@
+package corejava.grccodebase.ProgrammingElements.Level1;
+
+import java.util.Scanner;
+
+public class HeightConversion {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double cm = sc.nextDouble();
+
+        double totalInches = cm / 2.54;
+
+        int feet = (int)(totalInches / 12);
+        double inches = totalInches % 12;
+
+        System.out.println("Your Height in cm is " + cm +
+                " while in feet is " + feet +
+                " and inches is " + inches);
+    }
+}
---
+++ core-java-practice/gcr-codebase/Programming_Elements/level1/KmToMilesUserIP.java
@@ -0,0 +1,15 @@
+package corejava.grccodebase.ProgrammingElements.Level1;
+
+import java.util.Scanner;
+
+public class KmToMilesUserIP {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double km = sc.nextDouble();
+
+        double miles = km * 1.6;
+
+        System.out.println("The total miles is " + miles + " mile for the given " + km + " km");
+    }
+}
