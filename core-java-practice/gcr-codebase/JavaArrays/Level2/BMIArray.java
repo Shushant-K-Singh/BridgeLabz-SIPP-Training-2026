60334e32c66d04213b12503531746ec798e841ff
Shushant-K-Singh <shushantkumar860@gmail.com>
2026-06-10 19:34:44 +0530

    [Shushant-K-Singh].Add completed both levels of java Array

---
+++ core-java-practice/gcr-codebase/JavaArrays/Level2/BMIArray.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+
+public class BMIArray {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of persons: ");
+        int n = sc.nextInt();
+
+        double[] weight = new double[n];
+        double[] height = new double[n];
+        double[] bmi = new double[n];
+        String[] status = new String[n];
+
+        for (int i = 0; i < n; i++) {
+
+            System.out.print("Enter weight (kg): ");
+            weight[i] = sc.nextDouble();
+
+            System.out.print("Enter height (m): ");
+            height[i] = sc.nextDouble();
+        }
+
+        for (int i = 0; i < n; i++) {
+
+            bmi[i] = weight[i] / (height[i] * height[i]);
+
+            if (bmi[i] < 18.5) {
+                status[i] = "Underweight";
+            } else if (bmi[i] < 25) {
+                status[i] = "Normal";
+            } else if (bmi[i] < 30) {
+                status[i] = "Overweight";
+            } else {
+                status[i] = "Obese";
+            }
+        }
+
+        System.out.println("\nDetails");
+
+        for (int i = 0; i < n; i++) {
+
+            System.out.println("Person " + (i + 1));
+            System.out.println("Height = " + height[i]);
+            System.out.println("Weight = " + weight[i]);
+            System.out.println("BMI = " + bmi[i]);
+            System.out.println("Status = " + status[i]);
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
